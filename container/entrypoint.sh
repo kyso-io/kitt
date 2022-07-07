a27af38544114682acc59ff24e82bbf7d2a14186
Sergio Talens-Oliag <sto@kyso.io>
2022-07-07 16:50:06 +0200

    Update container passwd, shadow & group.

--- container/entrypoint.sh
+++ container/entrypoint.sh
@@ -13,7 +13,10 @@ if [ "$DEBUG" = "true" ]; then
 fi
 
 USER_NAME="${USER_NAME:-root}"
-USER_UID="$(id -u "$USER_NAME")"
+USER_UID="${USER_UID:-0}"
+USER_GID="${USER_GID:-0}"
+USER_HOME="${USER_HOME:-/}"
+USER_SHELL="${USER_SHELL:-/bin/bash}"
 
 if [ "$USER_UID" -eq "0" ]; then
   if [ "$*" ]; then
@@ -22,6 +25,10 @@ if [ "$USER_UID" -eq "0" ]; then
     exec /bin/bash -l
   fi
 else
+  echo "$USER_NAME:x:$USER_UID:$USER_GID:Kitt USER:$USER_HOME:$USER_SHELL" \
+    >>/etc/passwd || true
+  echo "$USER_NAME:::0:99999:7:::" >>/etc/shadow || true
+  echo "$USER_NAME:x:$USER_GID:" >>/etc/group || true
   echo "$USER_NAME ALL=(ALL) NOPASSWD:ALL" >/etc/sudoers.d/kitt-user
   # shellcheck disable=SC2086
   if [ "$*" ]; then
