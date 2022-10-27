1eeb9dd2e053cd10bdb5900abd9204b19941b5ca
Sergio Talens-Oliag <sto@kyso.io>
2022-10-27 16:58:57 +0200

    Check if tools are available.

--- lib/kitt/incl/ctools/k3d.sh
+++ lib/kitt/incl/ctools/k3d.sh
@@ -38,6 +38,8 @@ export APP_DEFAULT_CLUSTER_USE_REMOTE_REGISTRY="true"
 if [ -d "$INCL_DIR" ]; then
   # shellcheck source=../common.sh
   [ "$INCL_COMMON_SH" = "1" ] || . "$INCL_DIR/common.sh"
+  # shellcheck source=../tools.sh
+  [ "$INCL_TOOLS_SH" = "1" ] || . "$INCL_DIR/tools.sh"
 fi
 
 # ---------
@@ -218,6 +220,7 @@ EOF
 
 ctool_k3d_remove_cluster() {
   _cluster="$1"
+  tools_check_apps "k3d"
   ctool_k3d_export_variables "$_cluster"
   if k3d cluster ls --no-headers | grep -q "^${CLUSTER_NAME} "; then
     read_bool "Remove existing cluster?" "No"
@@ -244,6 +247,7 @@ ctool_k3d_remove_cluster() {
 # Installation related functions
 ctool_k3d_install() {
   _cluster="$1"
+  tools_check_apps "k3d" "kubectx" "kubectl"
   ctool_k3d_export_variables "$_cluster"
   # Remove old cluster?
   ctool_k3d_remove_cluster "$_cluster"
