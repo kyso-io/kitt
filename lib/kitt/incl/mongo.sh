7a8d0d49a8831eea942460a0d77d72f1e8924eb4
Sergio Talens-Oliag <sto@kyso.io>
2022-11-04 12:26:48 +0100

    fix(mongo): improvements for the mongo command
    Changes:
    
    - Use 'mongosh' with the 'cli' directly (it replaces 'mongo' and new
      default client image has it, so it will always be available)
    
    - Removed the 'run' command (no real use for it, replaced by the 'shell'
      command)
    
    - Added command 'settings-count' (counts the number of documents on the
      KysoSettings collection)
    
    - Added command 'shell' to run an interactive bash session on the client
      container
    
    - Improved help and command processing:
    
      - improved help messages for commands, the command/mongo script was
        adjusted to get the command names right for the new format
    
      - moved args processing and variariable exports to the mongo_command
        function to make functions that don't need an arguments work right
        ('settings-count', 'shell' and 'version-get') and export the right
        DEPLOYMENT and CLUSTER values if provided
    
      - print error messages with an explanation of what is missing when
        calling commands that need an ARG and it is not provided

--- lib/kitt/incl/mongo.sh
+++ lib/kitt/incl/mongo.sh
@@ -32,9 +32,21 @@ fi
 
 mongo_command() {
   _cmnd="$1"
-  _arg="$2"
-  _deployment="$3"
-  _cluster="$4"
+  case "$_cmnd" in
+  cli | dump | restore | settings-import | settings-merge | version-set)
+    _arg="$2"
+    _deployment="$3"
+    _cluster="$4"
+    ;;
+  shell | settings-count | version-get)
+    _deployment="$2"
+    _cluster="$3"
+    ;;
+  *)
+    echo "Unkown mongo command '$_cmnd', aborting"
+    exit 1
+    ;;
+  esac
   apps_mongodb_export_variables "$_deployment" "$_cluster"
   if ! find_namespace "$MONGODB_NAMESPACE"; then
     echo "MongoDB Namespace '$MONGODB_NAMESPACE' not found, can't run command"
@@ -47,20 +59,15 @@ mongo_command() {
   _user_database_uri="$(
     apps_mongodb_print_user_database_uri "$_deployment" "$_cluster"
   )"
-  _cmnd=""
   _file=""
-  case "$1" in
+  case "$_cmnd" in
   cli)
     case "$_arg" in
     root)
-      _cmnd="type mongosh >/dev/null 2>/dev/null"
-      _cmnd="$_cmnd && mongosh $_root_database_uri"
-      _cmnd="$_cmnd || mongo $_root_database_uri"
+      _script="mongosh $_root_database_uri"
       ;;
-    user | kyso | '')
-      _cmnd="type mongosh >/dev/null 2>/dev/null"
-      _cmnd="$_cmnd && mongosh $_user_database_uri"
-      _cmnd="$_cmnd || mongo $_user_database_uri"
+    kyso)
+      _script="mongosh $_user_database_uri"
       ;;
     *)
       echo "Unknown user '$_arg'"
@@ -70,47 +77,70 @@ mongo_command() {
     ;;
   dump)
     if [ "$_arg" ]; then
-      _cmnd="mongodump --uri $_user_database_uri --archive 2>/dev/null"
+      _script="mongodump --uri $_user_database_uri --archive 2>/dev/null"
       _fdir="stdout"
       _file="$_arg"
+    else
+      echo "Pass the FILE to dump the database contents to"
+      exit 1
     fi
     ;;
   restore)
     if [ "$_arg" ]; then
-      _cmnd="mongorestore --uri $_user_database_uri --archive --drop"
+      _script="mongorestore --uri $_user_database_uri --archive --drop"
       _file="stdin"
       _file="$_arg"
+    else
+      echo "Pass the database dump FILE to restore"
+      exit 1
     fi
-    ;;
-  run)
-    shift 1
-    _cmnd="$_arg"
     ;;
   settings-export)
     if [ "$_arg" ]; then
-      _cmnd="mongoexport --uri $_user_database_uri -c KysoSettings"
-      _cmnd="$_cmnd -f key,value --type=csv 2>/dev/null"
+      _script="mongoexport --uri $_user_database_uri -c KysoSettings"
+      _script="$_script -f key,value --type=csv 2>/dev/null"
       _fdir="stdout"
       _file="$_arg"
+    else
+      echo "Pass the CSV FILE to write settings to"
+      exit 1
     fi
     ;;
   settings-merge)
     if [ "$_arg" ]; then
-      _cmnd="mongoimport --uri $_user_database_uri -c KysoSettings"
-      _cmnd="$_cmnd --mode=merge --headerline --upsertFields=key --type=csv"
-      _cmnd="$_cmnd 2>/dev/null"
+      _script="mongoimport --uri $_user_database_uri -c KysoSettings"
+      _script="$_script --mode=merge --headerline --upsertFields=key --type=csv"
+      _script="$_script 2>/dev/null"
       _fdir="stdin"
       _file="$_arg"
+    else
+      echo "Pass the CSV FILE to merge from (first line must be 'key,value')"
+      exit 1
     fi
     ;;
+  settings-count)
+    _mongo_script="db.KysoSettings.countDocuments();"
+    _script="mongosh $_user_database_uri --quiet --eval '$_mongo_script'"
+    _fdir=""
+    _file="-"
+    ;;
+  shell)
+    _script="exec /bin/bash"
+    ;;
   version-get)
-    _mongo_cmnd="{ getParameter: 1, featureCompatibilityVersion: 1 }"
-    _cmnd="mongo $_root_database_uri --eval 'db.adminCommand($_mongo_cmnd)'"
+    _admin_command_args="{ getParameter: 1, featureCompatibilityVersion: 1 }"
+    _mongo_script="db.adminCommand($_admin_command_args);"
+    _script="mongosh $_root_database_uri --quiet --eval '$_mongo_script'"
+    _fdir=""
+    _file="-"
     ;;
   version-set)
     if [ "$_arg" ]; then
-      _mongo_cmnd="{ setFeatureCompatibilityVersion: \"$_arg\" }"
-      _cmnd="mongo $_root_database_uri --eval 'db.adminCommand($_mongo_cmnd)'"
+      _admin_command_args="{ setFeatureCompatibilityVersion: \"$_arg\" }"
+      _mongo_script="db.adminCommand($_admin_command_args);"
+      _script="mongosh $_root_database_uri --quiet --eval '$_mongo_script'"
+      _fdir=""
+      _file="-"
     else
       echo "Pass the compatibility version (i.e. '5.0')"
       exit 1
@@ -127,7 +157,7 @@ mongo_command() {
       --rm --stdin --tty --wait=true --restart='Never' \
       --env "MONGODB_KYSO_DATABASE_URI=$_user_database_uri" \
       --env "MONGODB_ROOT_DATABASE_URI=$_root_database_uri" \
-      --image "$MONGODB_CLI_IMAGE" --command -- /bin/sh -c "$_cmnd"
+      --image "$MONGODB_CLI_IMAGE" --command -- /bin/sh -c "$_script"
   else
     ret="0"
     kubectl run -n "$MONGODB_NAMESPACE" "$container_name" \
@@ -143,12 +173,20 @@ mongo_command() {
     fi
     if [ "$ret" -eq "0" ]; then
       if [ "$_fdir" = "stdout" ]; then
-        kubectl exec -n "$MONGODB_NAMESPACE" "$container_name" \
-          --stdin --quiet -- /bin/sh -c "$_cmnd" |
-          stdout_to_file "$_file" || ret="$?"
-      else
+        kubectl exec -n "$MONGODB_NAMESPACE" "$container_name" --stdin \
+          --quiet -- /bin/sh -c "$_script" | stdout_to_file "$_file" ||
+          ret="$?"
+      elif [ "$_fdir" = "stdin" ]; then
         file_to_stdout "$_file" | kubectl exec -n "$MONGODB_NAMESPACE" \
-          "$container_name" --stdin --quiet -- /bin/sh -c "$_cmnd" || ret="$?"
+          "$container_name" --stdin --quiet -- /bin/sh -c "$_script" ||
+          ret="$?"
+      elif [ "$_file" = "-" ] && [ "$_fdir" = "" ]; then
+        kubectl exec -n "$MONGODB_NAMESPACE" "$container_name" --quiet -- \
+          /bin/sh -c "$_script" ||
+          ret="$?"
+      else
+        echo "Wrong _file ('$_file') or _fdir ('$_fdir'), aborting call !!!"
+        exit 1
       fi
     fi
     kubectl delete -n "$MONGODB_NAMESPACE" "pod/$container_name" --now 1>&2
@@ -157,14 +195,15 @@ mongo_command() {
 
 mongo_command_list() {
   cat <<EOF
-cli: Run mongosh or the mongo client using 'root' or the 'kyso' user
-dump: Dump the kyso database to the passed file
-restore: Restore the kyso database from the passed file
-run: Run the given command on a containter (it is executed using /bin/sh -c)
-settings-export: Export the KysoSettings collection to a CSV file
-settings-merge: Merge the CSV data into the KysoSettings collection
-version-get: Get the current mongodb compatibility version
-version-set: Set the mongondb compatibility version
+cli USER: Run mongosh or the mongo client using 'root' or 'kyso' as USER
+dump FILE: Dump the kyso database to the passed FILE
+restore FILE: Restore the kyso database from the passed FILE
+settings-count: Get count of docs on the 'KysoSettings' collection [no ARG]
+settings-export FILE: Export the 'KysoSettings' collection to a CSV FILE
+settings-merge FILE: Merge the CSV FILE data into the 'KysoSettings' collection
+shell: Execute an interactive shell on the mongo client container [no ARG]
+version-get: Get the current mongodb compatibility version [no ARG]
+version-set VERSION: Set the mongondb compatibility version (i.e. 5.0)
 EOF
 }
 
