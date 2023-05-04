7833e6a082b5a25a32ae5e3942b246ac301655ba
Fran Barrena <francisco@kyso.io>
2023-05-04 19:54:40 +0200

    fix: removed terraform files

--- docs/deployment/eks-efs-aws-recommended-installation/terraform/eks/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "private_subnets" {
-  value       = module.vpc.private_subnets
-  description = "Private subnet ids"
-}
--- docs/deployment/eks-efs-aws-recommended-installation/terraform/state/config.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "kyso-tftest-terraform-858604803370"
-    key    = "tftest/state.tfstate"
-    region = "eu-west-1"
-  }
-}
